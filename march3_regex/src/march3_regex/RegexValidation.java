b30ca8ad01dfa024f8aed7fbe59ec9e991d8211f
Brandon Holman <bholman1990@gmail.com>
2021-03-03 17:12:51 -0700

    missing passport functionality

---
+++ march3_regex/src/march3_regex/RegexValidation.java
@@ -0,0 +1,26 @@
+package march3_regex;
+
+public class RegexValidation {
+	
+	public static void main(String[] args) {
+		
+		String passport = "12943568";
+		
+		
+		String SSN = "789-97-9859";
+		if(SSN.matches("[0-9]{3}-[0-9]{2}-[0-9]{4}")) {
+			System.out.println("SSN Valid");
+		}
+		else {
+			System.out.println("SSN invalid");
+		}
+		
+		String DL = "E4953785";
+		if(DL.matches("[A-Z]{1}[0-9]{7}")) {
+			System.out.println("Drivers Liscence Valid");
+		}
+		else {
+			System.out.println("Drivers Liscence Invalid");
+		}
+	}
+}
